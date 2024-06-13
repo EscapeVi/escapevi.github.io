900a906a6f8553c61fdc6a0b2a207ff45a0558bd
Elsie Clark <github@elsieclark.com>
2024-06-12 21:22:16 -0700

    Fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -189,7 +189,7 @@ function renderKnightControls(
 
 function determineIsCorrect(shapes: TwoDSet, volumes: ThreeDSet) {
   return volumes.every((volume, i) => {
-    return Math.log2(volume - shapes[i]) % 1 !== 0 && volume !== shapes[i] * 2;
+    return Math.log2(volume - shapes[i]) % 1 !== 0 && Math.log2(volume / 2) % 1 !== 0;
   });
 }
 
